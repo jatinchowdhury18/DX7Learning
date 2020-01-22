4c25f48dfa88f5c8ae3d6e29edf695251ee46c2f
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-01-22 12:15:38 -0800

    Use random MIDI notes and synth parameters

--- Gen_Data/DataGenerator/Source/Main.cpp
+++ Gen_Data/DataGenerator/Source/Main.cpp
@@ -14,19 +14,17 @@
 class ScopedFormatManager : public AudioFormatManager
 {
 public:
-    ScopedFormatManager()
-    {
-        registerBasicFormats();
-    }
-
-    ~ScopedFormatManager()
-    {
-        clearFormats();
-    }
+    ScopedFormatManager() { registerBasicFormats(); }
+    ~ScopedFormatManager() { clearFormats(); }
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ScopedFormatManager)    
 };
+
+int writeBufferToFile (ScopedFormatManager& formatManager, File& outFile,
+                       const AudioBuffer<float>& buffer, double sampleRate, int lengthSamples);
+void setRandomDexedParameters (DexedAudioProcessor& dexed);
+void randomMidiBuffer (MidiBuffer& midi, int lengthSamples);
 
 int main (int argc, char* argv[])
 {
@@ -34,6 +32,7 @@ int main (int argc, char* argv[])
     ScopedFormatManager formatManager; // audio format manager
 
     DexedAudioProcessor dexed; // create instance of dexed
+    int result = 0; // executable result
 
     // create output file
     File outFile (File::getCurrentWorkingDirectory().getChildFile ("test.wav"));
@@ -47,12 +46,13 @@ int main (int argc, char* argv[])
     const int lengthSamples = int (sampleRate * lengthSeconds);
 
     AudioBuffer<float> buffer (numChannels, lengthSamples);
-    
-    int noteNum = 60; //  C3
-    int midiChannel = 1;
+
+    // MIDI buffer
     MidiBuffer midi;
-    midi.addEvent (MidiMessage::noteOn  (midiChannel, noteNum, 0.5f), 0);
-    midi.addEvent (MidiMessage::noteOff (midiChannel, noteNum), int ((float) lengthSamples * 0.75f));
+    randomMidiBuffer (midi, lengthSamples);
+
+    // Set parameters
+    setRandomDexedParameters (dexed);    
 
     // synthesize buffer
     dexed.prepareToPlay ((double) sampleRate, lengthSamples);
@@ -60,8 +60,65 @@ int main (int argc, char* argv[])
     dexed.releaseResources();
 
     // write to file
+    result = writeBufferToFile (formatManager, outFile, buffer, (double) sampleRate, lengthSamples);
+
+    return result;
+}
+
+void randomMidiBuffer (MidiBuffer& midi, int lengthSamples)
+{
+    Random rand; // random number generator
+    const int midiChannel = 1;
+
+    int curSample = 0;
+    bool turnOn = true;
+    int noteNum = 0;
+    while (curSample < lengthSamples)
+    {
+        if (turnOn)
+        {
+            noteNum = rand.nextInt (Range<int> (24, 96)); // choose note between C2 and C7
+            midi.addEvent (MidiMessage::noteOn  (midiChannel, noteNum, 1.0f), curSample);
+        }
+        else
+        {
+            midi.addEvent (MidiMessage::noteOff (midiChannel, noteNum), curSample);
+        }
+
+        turnOn = ! turnOn;
+        curSample += rand.nextInt (Range<int> (1000, 40000));
+    }
+}
+
+void getParams (DexedAudioProcessor& dexed)
+{
+    for (auto* ctrl : dexed.ctrl)
+        std::cout << ctrl->label << ": " << ctrl->getValueHost() << std::endl;
+}
+
+void setRandomDexedParameters (DexedAudioProcessor& dexed)
+{
+    Random rand; // random number generator
+    auto carts = dexed.dexedCartDir.findChildFiles (File::TypesOfFileToFind::findFiles, true, "*.syx");
+
+    // load random cartridge
+    Cartridge thisCart;
+    thisCart.load (carts[rand.nextInt (carts.size())]);
+    dexed.loadCartridge (thisCart);
+
+    // load random program
+    int prog = rand.nextInt (32);
+    dexed.setCurrentProgram (prog);
+    std::cout << "Program: " << dexed.getProgramName (prog) << "\n\n";
+
+    getParams (dexed);
+}
+
+int writeBufferToFile (ScopedFormatManager& formatManager, File& outFile,
+                       const AudioBuffer<float>& buffer, double sampleRate, int lengthSamples)
+{
     std::unique_ptr<AudioFormatWriter> writer (formatManager.findFormatForFileExtension ("wav")->createWriterFor 
-        (outFile.createOutputStream(), (double) sampleRate, AudioChannelSet::stereo(), 16, StringPairArray(), 0));
+    (outFile.createOutputStream(), (double) sampleRate, AudioChannelSet::stereo(), 16, StringPairArray(), 0));
 
     if (writer.get() == nullptr)
     {
