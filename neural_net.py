221daa1304aba230fc15716451a9fff4ba5d9155
Jatin Chowdhury <jchowdhu@stanford.edu>
2020-02-29 23:34:06 -0800

    updated neural net

--- neural_net.py
+++ neural_net.py
@@ -1,20 +1,34 @@
 import tensorflow as tf
 from tensorflow.keras import models,layers,losses
 import numpy as np
-from scipy.io import loadmat
+from scipy.io import loadmat, wavfile
+import scipy.signal as signal
 import os
+import matplotlib.pyplot as plt
 
 # load data
-data = loadmat('xdata.mat')
-#X = data['xData'][:20,:3000]
-X = data['xData']
+# data = loadmat('xdata.mat')
+# X = data['xData'][:20,:3000]
+# X = data['xData']
+
+path = 'Data/Samples20k'
+N_samples = 5000 # 20000
+
+samples = []
+for n in range(N_samples):
+    fs, x = wavfile.read('{}/sample_{}.wav'.format(path, n)) # read file
+    x = x / 2**15                           # normalize
+    x = np.concatenate((x[:,0], x[:,1]))    # concatenate L+R channels
+    samples.append(x)
+
+X = np.asarray(samples)
 
 # load params
-params = (np.loadtxt('params.csv', delimiter=',')).astype(np.float)
+params = (np.loadtxt('Data/params20k.csv', delimiter=',')).astype(np.float)
 
 # set up x and y
 N = np.shape(X)[0]
-N_train = int(N*0.8)
+N_train = int(N*0.95)
 N_test = N - N_train
 
 # train set
@@ -23,25 +37,21 @@ train_y = params[:N_train,:]
 
 # test set
 test_X = X[N_train:,:]
-test_y = params[N_train:,:]
+test_y = params[N_train:N,:]
 
 print(np.shape(X))
 print(np.shape(train_X))
 print(np.shape(train_y))
-
-# Loss function
-class DX7Error(losses.Loss):
-    def call(self, y_true, y_pred):
-        print(y_true)
-        print(y_pred)
-        return 0
+print(np.shape(test_X))
+print(np.shape(test_y))
 
 # create tensorflow model
 model = models.Sequential()
 model.add(layers.InputLayer(input_shape=((X.shape[1],))))
-model.add(layers.Dense(8192, activation='relu'))
+model.add(layers.Dense(8*4096, activation='relu'))
+# model.add(layers.Dense(8192, activation='relu'))
 model.add(layers.Dense(4096, activation='relu'))
-model.add(layers.Dense(2048, activation='relu'))
+# model.add(layers.Dense(2048, activation='relu'))
 model.add(layers.Dense(1024, activation='relu'))
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(155, activation='sigmoid'))
@@ -49,37 +59,54 @@ model.add(layers.Dense(155, activation='sigmoid'))
 # compile and run model
 # using Mean Squared Error as loss function for now 
 # training on parameters for now
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # train model
-history = model.fit(train_X, train_y, epochs=100, validation_data=(test_X, test_y))
+history = model.fit(train_X, train_y, epochs=50, validation_data=(test_X, test_y))
 
-# test model with test samples
-# test_data = loadmat('xtestdata.mat')
-#X = test_data['xTestData'][:20,:3000]
-# X = test_data['xTestData']
+# plot losses
+train_loss = history.history['loss']
+test_loss =  history.history['val_loss']
+plt.plot(train_loss)
+plt.plot(test_loss)
+plt.savefig('Loss_mse.png')
+
+# Spectrogram loss function
+def spec_loss(x_wav, y_wav, fs):
+    nseg = 2048
+    _,_,Z_x = signal.stft(x_wav, fs=fs, nperseg=nseg, nfft=nseg*2, axis=0)
+    _,_,Z_y = signal.stft(y_wav, fs=fs, nperseg=nseg, nfft=nseg*2, axis=0)
+
+    loss = np.mean(np.abs(Z_x - Z_y), axis=None)
+    return loss
+
+def test_predictions(predictions, x_dir):
+    # get number of example and number of params
+    num_examples = predictions.shape[0]
+    num_params = predictions.shape[1]
+
+    # save output params to file
+    np.savetxt('params_out.csv', predictions, delimiter=',')
+    # run ParamsToAudio to get output audio files
+    os.system('./ParamsToAudio --outdir=TestOutputData --paramfile=params_out.csv > /dev/null 2>&1')
+
+    # Load files to compare
+    y_dir = 'TestOutputData'
+
+    total_loss = 0
+    for n in range(num_examples):
+        fs, x_wav = wavfile.read('{}/sample_{}.wav'.format(x_dir, n))
+        fs, y_wav = wavfile.read('{}/sample_{}.wav'.format(y_dir, n))
+
+        total_loss += spec_loss(x_wav, y_wav, fs)
+
+    return total_loss  
 
 # predict output params
-# predictions = model.predict(X)
+predictions = np.random.random_sample(np.shape(test_y))
+spectrogram_loss = test_predictions(predictions, path)
+print('Random loss: {}'.format(spectrogram_loss))
 
-# print(predictions)
-# print(type(predictions))
-
-# get number of example and number of params
-# num_examples = predictions.shape[0]
-# num_params = predictions.shape[1]
-
-# save output params to file
-# np.savetxt('params_out.csv', predictions, delimiter=',')
-# run ParamsToAudio to get output audio files
-# os.system('./ParamsToAudio --outdir=TestOutputData --paramfile=params_out.csv')
-
-# train_y = []
-# path = 'SamplesDir'
-# for n in range(N_train):
-#     train_y.append('{}/sample_{}.wav'.format(path,n))
-
-# test_y = []
-# path = 'TestDir'
-# for n in range(N_train, N):
-#     test_y.append('{}/sample_{}.wav'.format(path,n))
+predictions = model.predict(test_X)
+spectrogram_loss = test_predictions(predictions, path)
+print('Model loss: {}'.format(spectrogram_loss))
