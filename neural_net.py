37b3968f8876997d28e07630161660e7530c22b8
Jatin Chowdhury <jchowdhu@stanford.edu>
2020-03-09 14:47:33 -0700

    Start testing more advancer neural nets

--- neural_net.py
+++ neural_net.py
@@ -18,7 +18,7 @@ samples = []
 for n in range(N_samples):
     fs, x = wavfile.read('{}/sample_{}.wav'.format(path, n)) # read file
     x = x / 2**15                           # normalize
-    x = np.concatenate((x[:,0], x[:,1]))    # concatenate L+R channels
+    # x = np.concatenate((x[:,0], x[:,1]))    # concatenate L+R channels
     samples.append(x)
 
 X = np.asarray(samples)
@@ -46,89 +46,111 @@ print(np.shape(test_X))
 print(np.shape(test_y))
 
 # create tensorflow model
-model = models.Sequential()
-model.add(layers.InputLayer(input_shape=((X.shape[1],))))
-model.add(layers.Dense(8*4096, activation='relu'))
-# model.add(layers.Dense(8192, activation='relu'))
-model.add(layers.Dense(4096, activation='relu'))
-# model.add(layers.Dense(2048, activation='relu'))
-model.add(layers.Dense(1024, activation='relu'))
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(155, activation='sigmoid'))
+# model = models.Sequential()
+# model.add(layers.InputLayer(input_shape=((X.shape[1],))))
+# model.add(layers.Dense(8*4096, activation='relu'))
+# # model.add(layers.Dense(8192, activation='relu'))
+# model.add(layers.Dense(4096, activation='relu'))
+# # model.add(layers.Dense(2048, activation='relu'))
+# model.add(layers.Dense(1024, activation='relu'))
+# model.add(layers.Dense(256, activation='relu'))
+# model.add(layers.Dense(155, activation='sigmoid'))
 
 ''' POSSIBLE NEURAL NETWORKS TO USE '''
 ################################################################################################
 def rnn_model(input_dim, units, recur_layers, dropout_rate, output_dim=155):
-''' This RNN uses GRUs '''
-    input_data = layers.Input(name='input', shape = (input_dim,))  #shape=(None, input_dim))
+    ''' This RNN uses GRUs '''
+    input_data = layers.Input(name='input', shape=input_dim)
     
     for i in range(recur_layers):
         if i == 0:
-            gru = GRU(units, activation='relu', 
+            gru = layers.GRU(units, activation='relu', 
                     return_sequences=True, implementation=2, name='gru'+str(i))(input_data)
-            bn_rnn = BatchNormalization()(gru)
-            dropout_rnn = Dropout(dropout_rate)(bn_rnn)
+            bn_rnn = layers.BatchNormalization()(gru)
+            dropout_rnn = layers.Dropout(dropout_rate)(bn_rnn)
         else:
-            gru = GRU(units, activation='relu', 
+            gru = layers.GRU(units, activation='relu', 
                     return_sequences=True, implementation=2, name='gru'+str(i))(dropout_rnn)
-            bn_rnn = BatchNormalization()(gru)
-            dropout_rnn = Dropout(dropout_rate)(bn_rnn)
-            
-    time_dense = TimeDistributed(Dense(output_dim))(dropout_rnn)
+            bn_rnn = layers.BatchNormalization()(gru)
+            dropout_rnn = layers.Dropout(dropout_rate)(bn_rnn)
+    
+    # flat = layers.Flatten()(dropout_rnn)
+    time_dense = layers.TimeDistributed(layers.Dense(output_dim))(dropout_rnn) # layers.Dense(output_dim)(flat)
+    flat = layers.Flatten()(time_dense)
     # add sigmoid activation layer
-    y_pred = Activation('sigmoid', name='sigmoid')(time_dense)
+    y_pred = layers.Activation('sigmoid', name='sigmoid')(flat)
     
     model = models.Model(inputs=input_data, outputs=y_pred)
     print(model.summary())
     return model
 
 # model with 5 hidden layers
-model_1 = rnn_model(input_dim=X.shape[1], units=200, dropout_rate=0.8, recur_layers=4)
-
+# model_1 = rnn_model(input_dim=X.shape[1:], units=200, dropout_rate=1-0.8, recur_layers=4)
 
 ###################################################################################
 def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, units, output_dim=155):
-''' CNN + RNN '''
-    input_data = Input(name='input', shape = (input_dim,))
+    ''' CNN + RNN '''
+    input_data = layers.Input(name='input', shape = input_dim)
     
     # convolutional layer
-    conv_1d = Conv1D(filters, kernel_size, strides=conv_stride, padding=conv_border_mode, activation='relu',name='conv1d')(input_data)
+    conv_1d = layers.Conv1D(filters, kernel_size, strides=conv_stride, padding=conv_border_mode, activation='relu',name='conv1d')(input_data)
     # batch normalization
-    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    bn_cnn = layers.BatchNormalization(name='bn_conv_1d')(conv_1d)
+
+    # convolutional layer #2
+    conv_1d_2 = layers.Conv1D(filters, kernel_size, strides=conv_stride, padding=conv_border_mode, activation='relu',name='conv1d_2')(bn_cnn)
+    # batch normalization
+    bn_cnn_2 = layers.BatchNormalization(name='bn_conv_1d_2')(conv_1d_2)
+
+    # convolutional layer #3
+    conv_1d_2 = layers.Conv1D(filters, kernel_size, strides=conv_stride, padding=conv_border_mode, activation='relu',name='conv1d_3')(bn_cnn_2)
+    # batch normalization
+    bn_cnn_2 = layers.BatchNormalization(name='bn_conv_1d_3')(conv_1d_2)
+
     # recurrent layer
-    simp_rnn = SimpleRNN(units, activation='relu', return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+    simp_rnn = layers.SimpleRNN(units, activation='relu', return_sequences=True, implementation=2, name='rnn')(bn_cnn_2)
     # batch normalization
-    bn_rnn = BatchNormalization()(simp_rnn)
+    bn_rnn = layers.BatchNormalization()(simp_rnn)
+
+    # recurrent layer
+    simp_rnn = layers.SimpleRNN(units//4, activation='relu', return_sequences=True, implementation=2, name='rnn_2')(bn_rnn)
+    # batch normalization
+    bn_rnn = layers.BatchNormalization()(simp_rnn)
+
+    flat = layers.Flatten()(bn_rnn)
+    dense = layers.Dense(output_dim)(flat)
+
     # TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # time_dense = layers.TimeDistributed(layers.Dense(output_dim))(bn_rnn)
     
     # sigmoid activation layer
-    y_pred = Activation('sigmoid', name='sigmoid')(time_dense)
+    y_pred = layers.Activation('sigmoid', name='sigmoid')(dense)
 
     model = models.Model(inputs=input_data, outputs=y_pred)
     #model.output_length = lambda x: cnn_output_length(x, kernel_size, conv_border_mode, conv_stride)
     print(model.summary())
     return model
 
-model_2 = cnn_rnn_model(input_dim=X.shape[1],
+model_2 = cnn_rnn_model(input_dim=X.shape[1:],
                         filters=200,
                         kernel_size=11, 
                         conv_stride=2,
                         conv_border_mode='valid',
                         units=200)
+
 ###############################################################################
 def cnn_model(input_dim, output_dim):
-'''CNN for spectrogram'''
-'''in progress'''
-    model = Sequential()
-    model.add(Conv2D(input_dim, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    model.add(Conv2D(input_dim * 2, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.8))
-    model.add(Flatten())
-    model.add(Dense(input_dim * 4, activation='relu'))
-    model.add(Dropout(0.8))
-    model.add(Dense(output_dim, activation='sigmoid'))
+    '''CNN for spectrogram'''
+    '''in progress'''
+    model = models.Sequential()
+    model.add(layers.Conv2D(input_dim, kernel_size=(3, 3), activation='relu', input_shape=input_dim))
+    model.add(layers.Conv2D(input_dim * 2, kernel_size=(3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.8))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(input_dim * 4, activation='relu'))
+    model.add(layers.Dropout(0.8))
+    model.add(layers.Dense(output_dim, activation='sigmoid'))
 
     print(model.summary())
 ##############################################################################
@@ -136,17 +158,18 @@ def cnn_model(input_dim, output_dim):
 # compile and run model
 # using Mean Squared Error as loss function for now 
 # training on parameters for now
+model = model_2
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # train model
-history = model.fit(train_X, train_y, epochs=50, validation_data=(test_X, test_y))
+history = model.fit(train_X, train_y, epochs=10, validation_data=(test_X, test_y))
 
 # plot losses
 train_loss = history.history['loss']
 test_loss =  history.history['val_loss']
 plt.plot(train_loss)
 plt.plot(test_loss)
-plt.savefig('Loss_mse.png')
+plt.savefig('Loss.png')
 
 # Spectrogram loss function
 def spec_loss(x_wav, y_wav, fs):
